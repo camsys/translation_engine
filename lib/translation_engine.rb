ab242fb8f6fb8fc6e3a72fb08061b709282efb6e
Alex Bromley <evilbikes79@gmail.com>
2015-05-19 21:33:43 -0400

    Run engine migrations in parent apps.

--- lib/translation_engine.rb
+++ lib/translation_engine.rb
@@ -4,6 +4,17 @@ require 'tasks/load_locales'
 
 module TranslationEngine
 
+  class Engine < ::Rails::Engine
+    
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+  end
+
 end
 
 #Include as view helper
