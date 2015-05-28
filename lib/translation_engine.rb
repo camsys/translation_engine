1e11b5c56dcf77c2bc775d98ecdf473b645c256f
Alex Bromley <evilbikes79@gmail.com>
2015-05-28 12:05:22 -0400

    Increase fault tolerance in case of missing keys or other unforseen circumstances [#94010212]

--- lib/translation_engine.rb
+++ lib/translation_engine.rb
@@ -11,14 +11,17 @@ module TranslationEngine
     
   end
 
-  def self.translate_text key_param
-
-      locale = 'en'
-      translation_records = Translation.where("key = ? AND locale = ?", key_param, locale)
-      #should add a Honeybadger call if multiple records returned.
-      translation_records.count > 0 ? translation_text = translation_records[0].value : translation_text = "Translation not found: key = #{key_param}"
-      return translation_text
-
+  def self.translate_text(key_param)
+  	begin
+	    translation_key_id = TranslationKey.where("name = ?",key_param).first.id
+	    locale_id = Locale.where("name = ?",I18n.locale).first.id
+	    translation_records = Translation.where("translation_key_id = ? AND locale_id = ?", translation_key_id, locale_id)
+	    #should add a Honeybadger call if multiple records returned.
+	    translation_records.count > 0 ? translation_text = translation_records[0].value : translation_text = "Translation not found: key = #{key_param}"
+	    return translation_text
+    rescue
+      return "Translation not found: key = #{key_param}"
+    end
   end
 
 end
--- lib/translation_engine/action_view_extensions.rb
+++ lib/translation_engine/action_view_extensions.rb
@@ -3,16 +3,16 @@ module ActionViewExtensions
   extend ActiveSupport::Concern
 
   def translate_helper_translate(key_param)
-  	locale = extract_locale_from_subdomain
-  	translation_records = Translation.where("key = ? AND locale = ?", key_param, locale)
-  	#should add a Honeybadger call if multiple records returned.
-  	translation_records.count > 0 ? translation_text = translation_records[0].value : translation_text = "Translation not found: key = #{key_param}"
-  	return translation_text
-  end
-
-  def extract_locale_from_subdomain
-  	parsed_locale = request.fullpath.split("/")[1]
-	  I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : ""
+    begin
+      translation_key_id = TranslationKey.where("name = ?",key_param).first.id
+      locale_id = Locale.where("name = ?",I18n.locale).first.id
+      translation_records = Translation.where("translation_key_id = ? AND locale_id = ?", translation_key_id, locale_id)
+      #should add a Honeybadger call if multiple records returned.
+      translation_records.count > 0 ? translation_text = translation_records[0].value : translation_text = "Translation not found: key = #{key_param}"
+      return translation_text
+    rescue
+      return "Translation not found: key = #{key_param}"
+    end
   end
 
 end
