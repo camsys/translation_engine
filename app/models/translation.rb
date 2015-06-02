2c20e04fbcabd00c82a99dfd315445b24b0adf06
Alex Bromley <evilbikes79@gmail.com>
2015-06-01 23:07:08 -0400

    enforce unique locale-key concatenated key

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,4 +1,9 @@
 class Translation < ActiveRecord::Base
+
   belongs_to :locale
   belongs_to :translation_key
+
+  validates :locale_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
+  validates :translation_key_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
+
 end
--- lib/tasks/database_tasks.rb
+++ lib/tasks/database_tasks.rb
@@ -12,6 +12,9 @@ end
 class Translation < ActiveRecord::Base
   belongs_to :locale
   belongs_to :translation_key
+
+  validates :locale_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
+  validates :translation_key_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
 end
 
 namespace :translation_engine do
