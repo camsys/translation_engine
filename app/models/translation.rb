4280a528a0d6e8d6550f494a526dc6670f2b4a63
Alex Bromley <evilbikes79@gmail.com>
2015-06-24 14:38:34 -0400

    troubleshooting arc qa issue

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -3,7 +3,7 @@ class Translation < ActiveRecord::Base
   belongs_to :locale
   belongs_to :translation_key
 
-  validates :locale_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
-  validates :translation_key_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
+  #validates :locale_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
+  #validates :translation_key_id, :uniqueness => {:scope => [:locale_id, :translation_key_id]}
 
 end
