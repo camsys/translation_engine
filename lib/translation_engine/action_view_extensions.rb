8bcc82bd8926a4e132d1b0ff1f24929c9ce1d2af
Alex Bromley <evilbikes79@gmail.com>
2015-06-25 16:09:13 -0400

    cleanup

--- lib/translation_engine/action_view_extensions.rb
+++ lib/translation_engine/action_view_extensions.rb
@@ -4,16 +4,7 @@ module ActionViewExtensions
 
   def translate_helper(key_param, *interpolations)
     begin
-      # DRY DRY DRY
       return TranslationEngine.translate_text(key_param, interpolations)
-      # return "[" + key_param.to_s + "]" if I18n.locale == :tags
-      # translation_key_id = TranslationKey.where("name = ?",key_param.to_s).first.id
-      # locale_id = Locale.where("name = ?",I18n.locale).first.id
-      # translation_records = Translation.where("translation_key_id = ? AND locale_id = ?", translation_key_id, locale_id)
-      # #should add a Honeybadger call if multiple records returned.
-      # translation_records.count > 0 ? translation_text = translation_records[0].value : translation_text = "Translation not found: key = #{key_param}"
-      # translation_text.sub! '%{age}', '65'
-      # return translation_text.html_safe
     rescue err
       return "Translation not found: key = #{key_param}"
     end
