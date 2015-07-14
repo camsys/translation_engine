6641593f7990b2066a7b23524417ac0eb22708f7
Alex Bromley <evilbikes79@gmail.com>
2015-07-13 20:30:55 -0400

    WIP locale links [#98320976]

--- lib/translation_engine/action_view_extensions.rb
+++ lib/translation_engine/action_view_extensions.rb
@@ -10,4 +10,45 @@ module ActionViewExtensions
     return TranslationEngine.translate_array(array_of_keys)
   end
 
+  def links_to_each_locale(show_translations = false)
+
+    links = []
+    I18n.available_locales.each do |l|
+      links << link_using_locale(l)
+    end
+    if show_translations
+      links << link_using_locale(:tags)
+    end
+
+    return '' if links.size <= 1
+
+    links.join(' | ').html_safe
+    
+  end
+
+  def link_using_locale locale
+
+    path = request.fullpath
+    parts = path.split('/', 3)
+
+    current_locale = I18n.available_locales.detect do |l|
+      parts[1] == l.to_s
+    end
+    parts.delete_at(1) if current_locale or I18n.locale == :tags
+    parts = parts.join('/')
+    parts = '' if parts=='/'
+    newpath = "/#{locale}#{parts}"
+
+    if (newpath == path) or (newpath == "/#{I18n.locale}#{path}") or (newpath == "/#{I18n.locale}")
+      TranslationEngine.translate_text("locales.#{locale}")
+    else
+      if locale == :tags
+        link_to "Tags", newpath
+      else
+        link_to TranslationEngine.translate_text("locales.#{locale}"), newpath
+      end
+    end
+
+  end
+
 end
