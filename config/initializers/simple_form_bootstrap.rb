ec80cdfb04222c958cf1d942c46c07dc67fb0176
Alex Bromley <evilbikes79@gmail.com>
2015-07-16 10:12:06 -0400

    Include Simple Form and 1-Click style formatting [#99081634]

---
+++ config/initializers/simple_form_bootstrap.rb
@@ -0,0 +1,15 @@
+SimpleForm.setup do |config|
+
+  config.wrappers :horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'col-sm-3 control-label'
+
+    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+      ba.use :input, class: 'form-control'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+end
